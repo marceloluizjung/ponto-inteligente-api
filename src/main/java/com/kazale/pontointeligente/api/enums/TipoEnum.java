603f8d2ab0a87396e077c3bd737dd17451543c08
Marcelo <marcelo_4940@hotmail.com>
2019-04-10 19:09:03 -0300

    Aplicação em funcionamento.

--- src/main/java/com/kazale/pontointeligente/api/enums/TipoEnum.java
+++ src/main/java/com/kazale/pontointeligente/api/enums/TipoEnum.java
@@ -2,8 +2,9 @@ package com.kazale.pontointeligente.api.enums;
 
 public enum TipoEnum {
 	INICIO_TRABALHO,
-	TERMINO_TRABALHO, 
-	INICIO_ALMOCO, 
-	INICIO_PAUSA, 
+	TERMINO_TRABALHO,
+	INICIO_ALMOCO,
+	TERMINO_ALMOCO,
+	INICIO_PAUSA,
 	TERMINO_PAUSA
 }
